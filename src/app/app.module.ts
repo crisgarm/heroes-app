0eb1aba797cfaa3ad8a884e4fc6a1d6bd17be448
crisgarm <crisgarm9@gmail.com>
2021-04-06 15:27:43 +0200

    Create modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddComponent } from './pages/add/add.component';
+import { SearchComponent } from './pages/search/search.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroesListComponent } from './pages/heroes-list/heroes-list.component';
+
+
+
+@NgModule({
+  declarations: [AddComponent, SearchComponent, HeroeComponent, HomeComponent, HeroesListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroesModule { }

55ba2a82b88314291bc7f0bd9647c926ca6f480e
crisgarm <crisgarm9@gmail.com>
2021-04-07 19:02:53 +0200

    Install json server and inject a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/heroes/pages/heroes-list/heroes-list.component.ts
+++ src/app/heroes/pages/heroes-list/heroes-list.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './heroes-list.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeroesListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private heroesServices: HeroesService) {}
 
   ngOnInit(): void {
+    this.heroesServices.getHeroes().subscribe((resp) => console.log(resp));
   }
-
 }
